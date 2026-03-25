c66f17e7540548cd061b2db1dc2d7e4c67e2f46d
Ridoy-bushra <bushrabushra2710@gmail.com>
2026-03-25 21:45:27 +0600

    second commit

--- script/machine.js
+++ script/machine.js
@@ -12,3 +12,11 @@ function setBalance(value) {
     const balanceElement = document.getElementById("balance");
     balanceElement.innerText = value;
 }
+function showOnly(id){
+    const addMoney = document.getElementById("add-money");
+    const cashout = document.getElementById("cashout");
+    addMoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden");
+}
